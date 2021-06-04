0391220d8e6f76b15f0731a6c1347a39b56fc6fd
ryanpersad <Rpersad0305@gmail.com>
2021-06-04 06:16:46 -0400

    Initial commit

---
+++ src/main/java/Driving_Age/App.java
@@ -0,0 +1,27 @@
+package Driving_Age;
+import java.util.Scanner;
+
+/**
+ *  UCF COP3330 Summer 2021 Assignment 16 Solution
+ *  Copyright 2021 Ryan Persad
+ */
+
+public class App 
+{
+    static  Scanner myObj = new Scanner(System.in);
+
+    public static void main( String[] args )
+    {
+        String age;
+        int legal_age = 16;
+
+        System.out.println( "What is your age?" );
+        age = myObj.nextLine();
+        //convert to int
+        int new_age = Integer.parseInt(age);
+        //ternary operator to compare age
+        String result = (new_age >= legal_age)?"You are old enough to legally drive.":"You are not old enough to legally drive.";
+
+        System.out.println(result);
+    }
+}
